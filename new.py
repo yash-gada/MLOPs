76a0fa571a827ff6d52c1f07b5b51874d94b8b3d
Gada <yash.gada@capgemini.com>
2026-03-05 01:04:15 +0530

    Added streamlit code to new.py

--- new.py
+++ new.py
@@ -4,4 +4,13 @@ import matplotlib.pyplot as plt
 
 import streamlit as st
 
+from flask import Flask
+
 print("Hello, this is a new file for testing purposes!")
+
+st.title("Basic Streamlit Button Example")
+
+if st.button("Click me"):
+    st.write("Button was clicked!")
+else:
+    st.write("Waiting for you to click the button...")
